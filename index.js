1e3d87a1bb1b2d41d0982b0d62b038151153f9e2
LitStronger <1589015065@qq.com>
2020-09-03 15:08:13 +0800

    update

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const webp=require('webp-converter');
 webp.grant_permission();
 
-fs.readdir('./temp', function(err, files){
+fs.readdir('./input', function(err, files){
     if(err) throw err
     files.forEach(e => {
         let name = e.split('.')[0]
-        const result = webp.cwebp('./temp/'+e, name+".webp","-q 80");
+        const result = webp.cwebp('./input/'+e, './output/'+name+".webp","-q 80");
         result.then((response) => {
             console.log(response);
-          });
+        });
     });
 })
